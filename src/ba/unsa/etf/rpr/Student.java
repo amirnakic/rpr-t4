6f39a1833e10a57072555ade081b64efea2cf459
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 12:52:45 +0100

    Dodan je konstruktor klase Student.

--- src/ba/unsa/etf/rpr/Student.java
+++ src/ba/unsa/etf/rpr/Student.java
@@ -3,5 +3,11 @@ package ba.unsa.etf.rpr;
 public class Student {
     private String ime = "", prezime = "";
     private int brojIndeksa = 0;
+
+    public Student(String ime, String prezime, int brojIndeksa) {
+        this.ime = ime;
+        this.prezime = prezime;
+        this.brojIndeksa = brojIndeksa;
+    }
     
 }
