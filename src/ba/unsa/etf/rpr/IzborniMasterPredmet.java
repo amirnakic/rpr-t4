55bebab53126a53ea652f85c98fae30cf9646883
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 14:13:13 +0100

    Dodana je iz klase MasterPredmet izvedena klasa IzborniMasterPredmet, kao i njen konstruktor.

---
+++ src/ba/unsa/etf/rpr/IzborniMasterPredmet.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rpr;
+
+public class IzborniMasterPredmet extends MasterPredmet {
+    public IzborniMasterPredmet(String nazivPredmeta, String sifraPredmeta, int ects, int maxBrojStudenata) {
+        super(nazivPredmeta, sifraPredmeta, ects, maxBrojStudenata);
+    }
+    
+}
