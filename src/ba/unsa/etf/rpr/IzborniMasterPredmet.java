d51df2dfa651671dc95a84eb3a9c9b95103c5589
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 15:25:43 +0100

    Promjene unutar metode equals klase IzborniMasterPredmet.

--- src/ba/unsa/etf/rpr/IzborniMasterPredmet.java
+++ src/ba/unsa/etf/rpr/IzborniMasterPredmet.java
@@ -12,8 +12,8 @@ public class IzborniMasterPredmet extends MasterPredmet {
 
     @Override
     public boolean equals(Object o) {
-        if (o instanceof ObavezniMasterPredmet) {
-            ObavezniMasterPredmet omp = (ObavezniMasterPredmet) o;
+        if (o instanceof IzborniMasterPredmet) {
+            IzborniMasterPredmet omp = (IzborniMasterPredmet) o;
             if (omp.getNazivPredmeta().equals(this.getNazivPredmeta()) && omp.getSifraPredmeta().equals(this.getSifraPredmeta()) && omp.getECTS_BODOVI() == this.getECTS_BODOVI() && omp.getMAX_BROJ_STUDENATA() == this.getMAX_BROJ_STUDENATA())
                 return true;
         }
