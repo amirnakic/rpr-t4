97248343726778e56e07de1f95e65b6c3ee93367
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 13:42:47 +0100

    Dodan je atribut klasi MasterPredmet u vidu liste u kojoj će se čuvati studenti na tom predmetu, i korigovan je konstruktor ove klase.

--- src/ba/unsa/etf/rpr/MasterPredmet.java
+++ src/ba/unsa/etf/rpr/MasterPredmet.java
@@ -1,7 +1,15 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MasterPredmet extends Predmet {
+    private List<MasterStudent> studenti;
+
     public MasterPredmet(String nazivPredmeta, String sifraPredmeta, int ects, int maxBrojStudenata) {
         super(nazivPredmeta, sifraPredmeta, ects, maxBrojStudenata);
+        this.studenti = new ArrayList<>(this.getTrenutniBrojStudenata());
     }
+
+    
 }
