e2c9173e99c2997ed3c2ec6944741184fdb949bf
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 13:15:32 +0100

    Preklopljena je metoda equals unutar klase BachelorStudent.

--- src/ba/unsa/etf/rpr/BachelorStudent.java
+++ src/ba/unsa/etf/rpr/BachelorStudent.java
@@ -4,8 +4,19 @@ public class BachelorStudent extends Student {
     public BachelorStudent(String ime, String prezime, int brojIndeksa) {
         super(ime, prezime, brojIndeksa);
     }
+
     @Override
     public String ispisiStudenta() {
         return ("Bachelor" + super.ispisiStudenta());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof BachelorStudent) {
+            BachelorStudent bs = (BachelorStudent) o;
+            if (this.getIme().equals(bs.getIme()) && this.getPrezime().equals(bs.getPrezime()) && this.getBrojIndeksa() == bs.getBrojIndeksa())
+                return true;
+        }
+        return false;
     }
 }
