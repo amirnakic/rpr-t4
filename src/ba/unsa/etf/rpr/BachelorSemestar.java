73802cc9b93e2825e0ff671ce630d455e8b910af
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 16:57:21 +0100

    Dodana je metoda dodajPredmet unutar klase BachelorSemestar.

--- src/ba/unsa/etf/rpr/BachelorSemestar.java
+++ src/ba/unsa/etf/rpr/BachelorSemestar.java
@@ -20,4 +20,27 @@ public class BachelorSemestar extends Semestar {
     public List<IzborniBachelorPredmet> getIzborniPredmeti() {
         return izborniPredmeti;
     }
+
+    public void dodajPredmet(BachelorPredmet bp) throws IllegalArgumentException, ArrayStoreException {
+        if (bp instanceof ObavezniBachelorPredmet) {
+            if (getObavezniPredmeti().contains(bp)) throw new IllegalArgumentException(bp.ispisiPredmet() + " je već dodan u ovaj semestar.");
+            else if (getObavezniPredmeti().size() == getBROJ_OBAVEZNIH_PREDMETA()) throw new ArrayStoreException(bp.ispisiPredmet() + " nije dodan, jer je popunjen broj obaveznih predmeta u semestru.");
+            else {
+                getObavezniPredmeti().add((ObavezniBachelorPredmet)bp);
+                this.setTrenutniBrojObaveznihPredmeta(this.getTrenutniBrojObaveznihPredmeta() + 1);
+                this.setTrenutniBrojECTSBodova(this.getTrenutniBrojECTSBodova() + bp.getECTS_BODOVI());
+            }
+        }
+        else {
+            if (getIzborniPredmeti().contains(bp)) throw new IllegalArgumentException(bp.ispisiPredmet() + " je već dodan u ovaj semestar.");
+            else if (getIzborniPredmeti().size() == getBROJ_IZBORNIH_PREDMETA()) throw new ArrayStoreException(bp.ispisiPredmet() + " nije dodan, jer je popunjen broj obaveznih predmeta u semestru.");
+            else {
+                getIzborniPredmeti().add((IzborniBachelorPredmet)bp);
+                this.setTrenutniBrojIzbornihPredmeta(this.getTrenutniBrojIzbornihPredmeta() + 1);
+                this.setTrenutniBrojECTSBodova(this.getTrenutniBrojECTSBodova() + bp.getECTS_BODOVI());
+            }
+        }
+    }
+
+    
 }
