00b3a916cfbd69eb8624271fb98255952da5e225
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 14:14:14 +0100

    Preklopljena je metoda ispisiPredmet unutar klase IzborniMasterPredmet.

--- src/ba/unsa/etf/rpr/IzborniMasterPredmet.java
+++ src/ba/unsa/etf/rpr/IzborniMasterPredmet.java
@@ -4,5 +4,9 @@ public class IzborniMasterPredmet extends MasterPredmet {
     public IzborniMasterPredmet(String nazivPredmeta, String sifraPredmeta, int ects, int maxBrojStudenata) {
         super(nazivPredmeta, sifraPredmeta, ects, maxBrojStudenata);
     }
-    
+
+    @Override
+    public String ispisiPredmet() {
+        return ("IzborniMaster" + super.ispisiPredmet());
+    }
 }
