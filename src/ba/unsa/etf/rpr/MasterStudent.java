3da89a688abf2db4074f260ca6fd117365a3bd49
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 13:17:28 +0100

    Dodana je još jedna izvedena klasa iz klase Student - MasterStudent, kao i njen konstruktor.

---
+++ src/ba/unsa/etf/rpr/MasterStudent.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rpr;
+
+public class MasterStudent extends Student {
+    public MasterStudent(String ime, String prezime, int brojIndeksa) {
+        super(ime, prezime, brojIndeksa);
+    }
+    
+}
