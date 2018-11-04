bf6de326a2690effc6ffc4318ffbf93f2cee2925
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 17:35:52 +0100

    Dodana je metoda dodajSemestar unutar klase MasterCiklus.

--- src/ba/unsa/etf/rpr/MasterCiklus.java
+++ src/ba/unsa/etf/rpr/MasterCiklus.java
@@ -14,4 +14,14 @@ public class MasterCiklus extends Ciklus {
     public List<MasterSemestar> getMaster() {
         return master;
     }
+
+    public void dodajSemestar(MasterSemestar ms) throws IllegalArgumentException, ArrayStoreException {
+        if (getMaster().contains(ms)) throw new IllegalArgumentException(ms.ispisiSemestar() + " je vec dodan u ovaj ciklus.");
+        else if (getMaster().size() == this.getTrenutniBrojSemestara()) throw new ArrayStoreException(ms.ispisiSemestar() + " nije dodan, jer je popunjen kapacitet ovog ciklusa.");
+        else {
+            getMaster().add(ms);
+            this.setTrenutniBrojSemestara(this.getTrenutniBrojSemestara() + 1);
+        }
+    }
+
 }
