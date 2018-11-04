b7da21973a95593d9d2c60562b02e29303485efc
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 14:31:02 +0100

    Dodana je preklopljena metoda equals unutar klase ObavezniBachelorPredmet.

--- src/ba/unsa/etf/rpr/ObavezniBachelorPredmet.java
+++ src/ba/unsa/etf/rpr/ObavezniBachelorPredmet.java
@@ -9,4 +9,14 @@ public class ObavezniBachelorPredmet extends BachelorPredmet {
     public String ispisiPredmet() {
         return ("ObavezniBachelor" + super.ispisiPredmet());
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof ObavezniMasterPredmet) {
+            ObavezniMasterPredmet omp = (ObavezniMasterPredmet) o;
+            if (omp.getNazivPredmeta().equals(this.getNazivPredmeta()) && omp.getSifraPredmeta().equals(this.getSifraPredmeta()) && omp.getECTS_BODOVI() == this.getECTS_BODOVI() && omp.getMAX_BROJ_STUDENATA() == this.getMAX_BROJ_STUDENATA())
+                return true;
+        }
+        return false;
+    }
 }
