5cc3b849bb13b8d9e1928ad4cbd41e8a6b217923
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 14:02:00 +0100

    Dodana je iz klase MasterPredmet izvedena klasa ObavezniMasterPredmet, te njen konstruktor.

---
+++ src/ba/unsa/etf/rpr/ObavezniMasterPredmet.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rpr;
+
+public class ObavezniMasterPredmet extends MasterPredmet {
+    public ObavezniMasterPredmet(String nazivPredmeta, String sifraPredmeta, int ects, int maxBrojStudenata) {
+        super(nazivPredmeta, sifraPredmeta, ects, maxBrojStudenata);
+    }
+    
+}
