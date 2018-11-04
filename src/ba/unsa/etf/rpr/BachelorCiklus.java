de774608e6780eb11a8bf5ca5da708e52c6d9369
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 17:50:00 +0100

    Dodan je geter i metoda dodajSemestar unutar klase BachelorCiklus.

--- src/ba/unsa/etf/rpr/BachelorCiklus.java
+++ src/ba/unsa/etf/rpr/BachelorCiklus.java
@@ -10,4 +10,17 @@ public class BachelorCiklus extends Ciklus {
         super(brojSemestara);
         bachelor = new ArrayList<>(this.getTrenutniBrojSemestara());
     }
+
+    public List<BachelorSemestar> getBachelor() {
+        return bachelor;
+    }
+
+    public void dodajSemestar(BachelorSemestar bs) throws IllegalArgumentException, ArrayStoreException {
+        if (getBachelor().contains(bs)) throw new IllegalArgumentException(bs.ispisiSemestar() + " je već dodan u ovaj ciklus.");
+        else if (getBachelor().size() == this.getTrenutniBrojSemestara()) throw new ArrayStoreException(bs.ispisiSemestar() + " nije dodan, jer je popunjen kapacitet ovog ciklusa.");
+        else {
+            getBachelor().add(bs);
+            this.setTrenutniBrojSemestara(this.getTrenutniBrojSemestara() + 1);
+        }
+    }
 }
