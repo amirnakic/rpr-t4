1d3c37565383ee6f990e26bc366eb755ea21035c
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 14:32:51 +0100

    Dodana je iz BachelorPredmet klase izvedena klasa IzborniBachelorPredmet.

---
+++ src/ba/unsa/etf/rpr/IzborniBachelorPredmet.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rpr;
+
+public class IzborniBachelorPredmet extends BachelorPredmet {
+    public IzborniBachelorPredmet(String nazivPredmeta, String sifraPredmeta, int ects, int maxBrojStudenata) {
+        super(nazivPredmeta, sifraPredmeta, ects, maxBrojStudenata);
+    }
+    
+}
