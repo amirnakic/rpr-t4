58ddea1076531c31518382094b2813466b1617da
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 13:18:14 +0100

    Dodana je preklopljena metoda ispisiStudenta unutar klase MasterStudent.

--- src/ba/unsa/etf/rpr/MasterStudent.java
+++ src/ba/unsa/etf/rpr/MasterStudent.java
@@ -4,5 +4,9 @@ public class MasterStudent extends Student {
     public MasterStudent(String ime, String prezime, int brojIndeksa) {
         super(ime, prezime, brojIndeksa);
     }
-    
+
+    @Override
+    public String ispisiStudenta() {
+        return ("Master" + super.ispisiStudenta());
+    }
 }
