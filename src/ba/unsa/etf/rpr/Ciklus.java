d9153db80b381b03d9365bd0f52c48854d7538b8
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 17:14:40 +0100

    Dodani su atributi i konstruktor klase Ciklus.

--- src/ba/unsa/etf/rpr/Ciklus.java
+++ src/ba/unsa/etf/rpr/Ciklus.java
@@ -1,4 +1,11 @@
 package ba.unsa.etf.rpr;
 
 public class Ciklus {
+    protected int trenutniBrojSemestara = 0;
+    private final int BROJ_SEMESTARA;
+
+    public Ciklus(int brojSemestara) {
+        this.BROJ_SEMESTARA = brojSemestara;
+        this.trenutniBrojSemestara = 0;
+    }
 }
