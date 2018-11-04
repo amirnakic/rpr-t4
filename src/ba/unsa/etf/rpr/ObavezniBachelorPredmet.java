366792d1ed77f182939911b3b2ee8a6a05beefe1
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 14:29:23 +0100

    Dodana je iz BachelorPredmet klase izvedena klasa ObavezniBachelorPredmet, kao i njen konstruktor.

---
+++ src/ba/unsa/etf/rpr/ObavezniBachelorPredmet.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rpr;
+
+public class ObavezniBachelorPredmet extends BachelorPredmet {
+    public ObavezniBachelorPredmet(String nazivPredmeta, String sifraPredmeta, int ects, int maxBrojStudenata) {
+        super(nazivPredmeta, sifraPredmeta, ects, maxBrojStudenata);
+    }
+    
+}
