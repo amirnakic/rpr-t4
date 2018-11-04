3f601e32d71ea7be9df9cd0a17a252de2e9e5303
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 13:01:13 +0100

    Dodana je izvedena klasa BachelorStudent, i njen konstruktor.

---
+++ src/ba/unsa/etf/rpr/BachelorStudent.java
@@ -0,0 +1,7 @@
+package ba.unsa.etf.rpr;
+
+public class BachelorStudent extends Student {
+    public BachelorStudent(String ime, String prezime, int brojIndeksa) {
+        super(ime, prezime, brojIndeksa);
+    }
+}
