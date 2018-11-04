59f8c2a5b1aad6e1e94f8a07fb3a3781a60933c5
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 12:53:15 +0100

    Dodani su geteri/seteri klase Student.

--- src/ba/unsa/etf/rpr/Student.java
+++ src/ba/unsa/etf/rpr/Student.java
@@ -10,4 +10,27 @@ public class Student {
         this.brojIndeksa = brojIndeksa;
     }
     
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public int getBrojIndeksa() {
+        return brojIndeksa;
+    }
+
+    public void setBrojIndeksa(int brojIndeksa) {
+        this.brojIndeksa = brojIndeksa;
+    }
 }
