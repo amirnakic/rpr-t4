55747d660ffa45e24f9797dffb26c6d70b5890da
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 16:38:55 +0100

    Dodana je metoda brisiPredmet unutar klase MasterSemestar.

--- src/ba/unsa/etf/rpr/MasterSemestar.java
+++ src/ba/unsa/etf/rpr/MasterSemestar.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.rpr;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class MasterSemestar extends Semestar {
@@ -39,6 +40,35 @@ public class MasterSemestar extends Semestar {
                 this.setTrenutniBrojIzbornihPredmeta(this.getTrenutniBrojIzbornihPredmeta() + 1);
                 this.setTrenutniBrojECTSBodova(this.getTrenutniBrojECTSBodova() + mp.getECTS_BODOVI());
             }
+        }
+    }
+
+    public void brisiPredmet(MasterPredmet mp) throws IllegalArgumentException {
+        if (mp instanceof ObavezniMasterPredmet) {
+            Iterator it = getObavezniPredmeti().iterator();
+            while (it.hasNext()) {
+                ObavezniMasterPredmet omp1 = (ObavezniMasterPredmet) it.next();
+                if(omp1.equals(mp)) {
+                    getObavezniPredmeti().remove(mp);
+                    setTrenutniBrojObaveznihPredmeta(this.getTrenutniBrojObaveznihPredmeta() - 1);
+                    setTrenutniBrojECTSBodova(this.getUKUPAN_BROJ_ECTS_BODOVA() - mp.getECTS_BODOVI());
+                    return;
+                }
+            }
+            throw new IllegalArgumentException(mp.ispisiPredmet() + " nije dodan u ovaj semestar.");
+        }
+        else {
+            Iterator it = getIzborniPredmeti().iterator();
+            while (it.hasNext()) {
+                IzborniMasterPredmet imp1 = (IzborniMasterPredmet) it.next();
+                if(imp1.equals(mp)) {
+                    getIzborniPredmeti().remove(mp);
+                    setTrenutniBrojIzbornihPredmeta(this.getTrenutniBrojIzbornihPredmeta() - 1);
+                    setTrenutniBrojECTSBodova(this.getUKUPAN_BROJ_ECTS_BODOVA() - mp.getECTS_BODOVI());
+                    return;
+                }
+            }
+            throw new IllegalArgumentException(mp.ispisiPredmet() + " nije dodan u ovaj semestar.");
         }
     }
 }
