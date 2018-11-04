a7e46da28158934614439f94b43b15a71d6a1c4e
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 14:30:31 +0100

    Dodana je preklopljena metoda ispisiPredmet unutar klase ObavezniBachelorPredmet.

--- src/ba/unsa/etf/rpr/ObavezniBachelorPredmet.java
+++ src/ba/unsa/etf/rpr/ObavezniBachelorPredmet.java
@@ -4,5 +4,9 @@ public class ObavezniBachelorPredmet extends BachelorPredmet {
     public ObavezniBachelorPredmet(String nazivPredmeta, String sifraPredmeta, int ects, int maxBrojStudenata) {
         super(nazivPredmeta, sifraPredmeta, ects, maxBrojStudenata);
     }
-    
+
+    @Override
+    public String ispisiPredmet() {
+        return ("ObavezniBachelor" + super.ispisiPredmet());
+    }
 }
