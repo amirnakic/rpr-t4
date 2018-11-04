3d1bcb8fae524fdf1b245ddf8fe3ae9942b8d88f
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 17:28:19 +0100

    Dodana je preklopljena metoda equals unutar klase MasterSemestar.

--- src/ba/unsa/etf/rpr/MasterSemestar.java
+++ src/ba/unsa/etf/rpr/MasterSemestar.java
@@ -30,19 +30,22 @@ public class MasterSemestar extends Semestar {
 
     public void dodajPredmet(MasterPredmet mp) throws IllegalArgumentException, ArrayStoreException {
         if (mp instanceof ObavezniMasterPredmet) {
-            if (getObavezniPredmeti().contains(mp)) throw new IllegalArgumentException(mp.ispisiPredmet() + " je već dodan u ovaj semestar.");
-            else if (getObavezniPredmeti().size() == getBROJ_OBAVEZNIH_PREDMETA()) throw new ArrayStoreException(mp.ispisiPredmet() + " nije dodan, jer je popunjen broj obaveznih predmeta u semestru.");
+            if (getObavezniPredmeti().contains(mp))
+                throw new IllegalArgumentException(mp.ispisiPredmet() + " je već dodan u ovaj semestar.");
+            else if (getObavezniPredmeti().size() == getBROJ_OBAVEZNIH_PREDMETA())
+                throw new ArrayStoreException(mp.ispisiPredmet() + " nije dodan, jer je popunjen broj obaveznih predmeta u semestru.");
             else {
-                getObavezniPredmeti().add((ObavezniMasterPredmet)mp);
+                getObavezniPredmeti().add((ObavezniMasterPredmet) mp);
                 this.setTrenutniBrojObaveznihPredmeta(this.getTrenutniBrojObaveznihPredmeta() + 1);
                 this.setTrenutniBrojECTSBodova(this.getTrenutniBrojECTSBodova() + mp.getECTS_BODOVI());
             }
-        }
-        else {
-            if (getIzborniPredmeti().contains(mp)) throw new IllegalArgumentException(mp.ispisiPredmet() + " je već dodan u ovaj semestar.");
-            else if (getIzborniPredmeti().size() == getBROJ_IZBORNIH_PREDMETA()) throw new ArrayStoreException(mp.ispisiPredmet() + " nije dodan, jer je popunjen broj obaveznih predmeta u semestru.");
+        } else {
+            if (getIzborniPredmeti().contains(mp))
+                throw new IllegalArgumentException(mp.ispisiPredmet() + " je već dodan u ovaj semestar.");
+            else if (getIzborniPredmeti().size() == getBROJ_IZBORNIH_PREDMETA())
+                throw new ArrayStoreException(mp.ispisiPredmet() + " nije dodan, jer je popunjen broj obaveznih predmeta u semestru.");
             else {
-                getIzborniPredmeti().add((IzborniMasterPredmet)mp);
+                getIzborniPredmeti().add((IzborniMasterPredmet) mp);
                 this.setTrenutniBrojIzbornihPredmeta(this.getTrenutniBrojIzbornihPredmeta() + 1);
                 this.setTrenutniBrojECTSBodova(this.getTrenutniBrojECTSBodova() + mp.getECTS_BODOVI());
             }
@@ -59,7 +62,7 @@ public class MasterSemestar extends Semestar {
             Iterator it = getObavezniPredmeti().iterator();
             while (it.hasNext()) {
                 ObavezniMasterPredmet omp1 = (ObavezniMasterPredmet) it.next();
-                if(omp1.equals(mp)) {
+                if (omp1.equals(mp)) {
                     getObavezniPredmeti().remove(mp);
                     setTrenutniBrojObaveznihPredmeta(this.getTrenutniBrojObaveznihPredmeta() - 1);
                     setTrenutniBrojECTSBodova(this.getUKUPAN_BROJ_ECTS_BODOVA() - mp.getECTS_BODOVI());
@@ -67,12 +70,11 @@ public class MasterSemestar extends Semestar {
                 }
             }
             throw new IllegalArgumentException(mp.ispisiPredmet() + " nije dodan u ovaj semestar.");
-        }
-        else {
+        } else {
             Iterator it = getIzborniPredmeti().iterator();
             while (it.hasNext()) {
                 IzborniMasterPredmet imp1 = (IzborniMasterPredmet) it.next();
-                if(imp1.equals(mp)) {
+                if (imp1.equals(mp)) {
                     getIzborniPredmeti().remove(mp);
                     setTrenutniBrojIzbornihPredmeta(this.getTrenutniBrojIzbornihPredmeta() - 1);
                     setTrenutniBrojECTSBodova(this.getUKUPAN_BROJ_ECTS_BODOVA() - mp.getECTS_BODOVI());
@@ -93,5 +95,15 @@ public class MasterSemestar extends Semestar {
         for (ObavezniMasterPredmet omp : getObavezniPredmeti()) rezultat += omp.ispisiPredmet() + "\n";
         for (IzborniMasterPredmet imp : getIzborniPredmeti()) rezultat += imp.ispisiPredmet() + "\n";
         return rezultat;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof MasterSemestar) {
+            MasterSemestar bs = (MasterSemestar) o;
+            if (bs.getBrojSemestra() == this.getBrojSemestra())
+                return true;
+        }
+        return false;
     }
 }
