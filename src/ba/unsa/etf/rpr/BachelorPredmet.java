9999c0a87a2d2d4356d8b1997e647af3614ce7c3
amirnakic <anakic1@etf.unsa.ba>
2018-11-04 14:18:51 +0100

    Dodana je klasa BachelorPredmet izvedena iz klase Predmet, te njen privatni atribut i konstruktor.

---
+++ src/ba/unsa/etf/rpr/BachelorPredmet.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rpr;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BachelorPredmet extends Predmet {
+    private List<MasterStudent> studenti;
+
+    public MasterPredmet(String nazivPredmeta, String sifraPredmeta, int ects, int maxBrojStudenata) {
+        super(nazivPredmeta, sifraPredmeta, ects, maxBrojStudenata);
+        this.studenti = new ArrayList<>(this.getTrenutniBrojStudenata());
+    }
+}
