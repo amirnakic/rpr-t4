3c37465811eda0ec78176946b85e5f266266dc1d
amirnakic <anakic1@etf.unsa.ba>
2018-11-11 14:10:41 +0100

    Korigovan je konstruktor klase Predmet.

--- src/ba/unsa/etf/rpr/novo/Predmet.java
+++ src/ba/unsa/etf/rpr/novo/Predmet.java
@@ -1,12 +1,20 @@
 package ba.unsa.etf.rpr.novo;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Predmet {
     private String imePredmeta;
     private int ects, brojSemestra;
     private final int MAX_BROJ_STUDENATA = 60;
-    private List<Student> studentiNaPredmetu;
+    private List<Student> studentiNaPredmetu = new ArrayList<>();
+
+    public Predmet(String imePredmeta, int ects, int brojSemestra, List<Student> studenti) {
+        setImePredmeta(imePredmeta);
+        setEcts(ects);
+        setBrojSemestra(brojSemestra);
+        setStudentiNaPredmetu(studenti);
+    }
 
     public String getImePredmeta() {
         return imePredmeta;
