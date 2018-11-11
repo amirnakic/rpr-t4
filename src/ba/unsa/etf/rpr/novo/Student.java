1e130479df67fe6e89a598592807b9f7e458517a
amirnakic <anakic1@etf.unsa.ba>
2018-11-11 12:40:01 +0100

    Dodani su getteri/setteri klase Student.

--- src/ba/unsa/etf/rpr/novo/Student.java
+++ src/ba/unsa/etf/rpr/novo/Student.java
@@ -4,5 +4,27 @@ public class Student {
     private String ime, prezime;
     private int brojIndeksa;
 
-    
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public int getBrojIndeksa() {
+        return brojIndeksa;
+    }
+
+    public void setBrojIndeksa(int brojIndeksa) {
+        this.brojIndeksa = brojIndeksa;
+    }
 }
