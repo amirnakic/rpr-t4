74e205019ae54ba906b95f17f773b01b9f2b7687
amirnakic <anakic1@etf.unsa.ba>
2018-11-11 13:27:58 +0100

    Dodani su privatni atributi klase Predmet.

--- src/ba/unsa/etf/rpr/novo/Predmet.java
+++ src/ba/unsa/etf/rpr/novo/Predmet.java
@@ -1,4 +1,11 @@
 package ba.unsa.etf.rpr.novo;
 
+import java.util.List;
+
 public class Predmet {
+    private String imePredmeta;
+    private int ects, brojSemestra;
+    private final int MAX_BROJ_STUDENATA = 60;
+    private List<Student> studentiNaPredmetu;
+
 }
