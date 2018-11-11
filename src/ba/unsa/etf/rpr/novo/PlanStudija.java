c1136e15ea3ad804e3d676bf911de42e1844b945
amirnakic <anakic1@etf.unsa.ba>
2018-11-11 14:23:54 +0100

    Korekcije privatnog atributa realizovanog u obliku mape, te odgovarajućih gettera/settera klase PlanStudija.

--- src/ba/unsa/etf/rpr/novo/PlanStudija.java
+++ src/ba/unsa/etf/rpr/novo/PlanStudija.java
@@ -1,4 +1,25 @@
 package ba.unsa.etf.rpr.novo;
 
+import java.util.List;
+import java.util.Map;
+
 public class PlanStudija {
+    private String smjer;
+    Map<Integer, List<Student>> planStudija;
+
+    public String getSmjer() {
+        return smjer;
+    }
+
+    public void setSmjer(String smjer) {
+        this.smjer = smjer;
+    }
+
+    public Map<Integer, List<Student>> getPlanStudija() {
+        return planStudija;
+    }
+
+    public void setPlanStudija(Map<Integer, List<Student>> planStudija) {
+        this.planStudija = planStudija;
+    }
 }
