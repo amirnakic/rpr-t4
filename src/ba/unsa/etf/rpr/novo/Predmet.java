c6df04797456c727911a035ef2e950f767a5308c
amirnakic <anakic1@etf.unsa.ba>
2018-11-11 13:29:10 +0100

    Dodani su getteri/setteri klase Predmet.

--- src/ba/unsa/etf/rpr/novo/Predmet.java
+++ src/ba/unsa/etf/rpr/novo/Predmet.java
@@ -8,4 +8,39 @@ public class Predmet {
     private final int MAX_BROJ_STUDENATA = 60;
     private List<Student> studentiNaPredmetu;
 
+    public String getImePredmeta() {
+        return imePredmeta;
+    }
+
+    public void setImePredmeta(String imePredmeta) {
+        this.imePredmeta = imePredmeta;
+    }
+
+    public int getEcts() {
+        return ects;
+    }
+
+    public void setEcts(int ects) {
+        this.ects = ects;
+    }
+
+    public int getBrojSemestra() {
+        return brojSemestra;
+    }
+
+    public void setBrojSemestra(int brojSemestra) {
+        this.brojSemestra = brojSemestra;
+    }
+
+    public int getMAX_BROJ_STUDENATA() {
+        return MAX_BROJ_STUDENATA;
+    }
+
+    public List<Student> getStudentiNaPredmetu() {
+        return studentiNaPredmetu;
+    }
+
+    public void setStudentiNaPredmetu(List<Student> studentiNaPredmetu) {
+        this.studentiNaPredmetu = studentiNaPredmetu;
+    }
 }
