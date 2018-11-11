20925b5696841af46254fdd5479702bdbe36e494
amirnakic <anakic1@etf.unsa.ba>
2018-11-11 14:40:50 +0100

    Dodani su getteri/setteri klase Upis.

--- src/ba/unsa/etf/rpr/novo/Upis.java
+++ src/ba/unsa/etf/rpr/novo/Upis.java
@@ -5,5 +5,27 @@ public class Upis {
     private Predmet predmet;
     private PlanStudija planStudija;
 
-    
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Predmet getPredmet() {
+        return predmet;
+    }
+
+    public void setPredmet(Predmet predmet) {
+        this.predmet = predmet;
+    }
+
+    public PlanStudija getPlanStudija() {
+        return planStudija;
+    }
+
+    public void setPlanStudija(PlanStudija planStudija) {
+        this.planStudija = planStudija;
+    }
 }
