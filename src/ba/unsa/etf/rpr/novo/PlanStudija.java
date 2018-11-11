90412929ce8a52d978cfdfe30843a55bba1f49f2
amirnakic <anakic1@etf.unsa.ba>
2018-11-11 17:57:08 +0100

    Iz klase PlanStudija uklonjene su metode koje sam ranije imao, a kasnije zaključio da mi nisu potrebne.

--- src/ba/unsa/etf/rpr/novo/PlanStudija.java
+++ src/ba/unsa/etf/rpr/novo/PlanStudija.java
@@ -10,29 +10,6 @@ public class PlanStudija {
         setPlanStudija(studenti);
     }
 
-    public void dodajPredmet(Predmet p) throws IllegalArgumentException {
-        if (getPlanStudija().get(p.getBrojSemestra()) == null)
-            throw new IllegalArgumentException("Predmet ne pripada ovom planu studija.");
-        List<Predmet> predmeti = getPlanStudija().get(p.getBrojSemestra());
-        predmeti.add(p);
-    }
-
-    public void ukloniPredmet(Predmet p) throws IllegalArgumentException {
-        List<Predmet> predmeti = getPlanStudija().get(p.getBrojSemestra());
-        if (predmeti.contains(p))
-            predmeti.remove(p);
-        else throw new IllegalArgumentException("Nepostojeći predmet.");
-    }
-
-    public int dajBrojEctsBodovaUSemestru(int brojSemestra) throws IllegalArgumentException {
-        int brojEctsBodovaUSemestru = 0;
-        if (getPlanStudija().get(brojSemestra) == null)
-            throw new IllegalArgumentException("Semestar nije evidentiran.");
-        for (Predmet p : getPlanStudija().get(brojSemestra))
-            brojEctsBodovaUSemestru += p.getEcts();
-        return brojEctsBodovaUSemestru;
-    }
-
     public Map<Integer, List<Predmet>> getPlanStudija() {
         return planStudija;
     }
