cddc9cebb97ccfdbfe6ba57b727adda97bd48125
amirnakic <anakic1@etf.unsa.ba>
2018-11-11 14:20:09 +0100

    Uklonjen je atribut brojSemestra iz klase Predmet.

--- src/ba/unsa/etf/rpr/novo/Predmet.java
+++ src/ba/unsa/etf/rpr/novo/Predmet.java
@@ -5,14 +5,13 @@ import java.util.ArrayList;
 
 public class Predmet {
     private String imePredmeta;
-    private int ects, brojSemestra;
+    private int ects;
     private final int MAX_BROJ_STUDENATA = 60;
     private List<Student> studentiNaPredmetu = new ArrayList<>();
 
-    public Predmet(String imePredmeta, int ects, int brojSemestra, List<Student> studenti) {
+    public Predmet(String imePredmeta, int ects, List<Student> studenti) {
         setImePredmeta(imePredmeta);
         setEcts(ects);
-        setBrojSemestra(brojSemestra);
         setStudentiNaPredmetu(studenti);
     }
 
@@ -30,14 +29,6 @@ public class Predmet {
 
     public void setEcts(int ects) {
         this.ects = ects;
-    }
-
-    public int getBrojSemestra() {
-        return brojSemestra;
-    }
-
-    public void setBrojSemestra(int brojSemestra) {
-        this.brojSemestra = brojSemestra;
     }
 
     public int getMAX_BROJ_STUDENATA() {
